1fdf51fb706bb89f89a321cf7c8300b3eecefc43
Magesh <magesh@hash14.com>
2018-12-16 23:12:47 +0530

    Added logger function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers/todos";
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const logger = store => next => action => {
+  console.log(store.getState());
+  console.log("will dispatch", action);
+  return next(action);
+};
+
+// const logger = function(store) {
+//   return function(next) {
+//     return function(action) {
+//       console.log(store.getState());
+//       console.log("will dispatch", action);
+//       return next(action);
+//     };
+//   };
+// };
+const store = createStore(reducer, applyMiddleware(logger));
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
