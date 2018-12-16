5f6ab1a4ec74d3518e22a4ba5d83cc1f38d47b3f
Magesh <magesh@hash14.com>
2018-12-16 23:15:42 +0530

    logging next state

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers/todos";
 
-const logger = store => next => action => {
-  console.log(store.getState());
-  console.log("will dispatch", action);
-  return next(action);
+const logger = store => {
+  return next => {
+    return action => {
+      console.log("[Middleware] Dispatching", action);
+      const result = next(action);
+      console.log("[Middleware] next state", store.getState());
+      return result;
+    };
+  };
 };
-
 // const logger = function(store) {
 //   return function(next) {
 //     return function(action) {
